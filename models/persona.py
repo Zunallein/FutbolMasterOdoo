a3cb9e6177e707cb5ff471307b445c7e7815bb4d
Zunallein <zunallein@gmail.com>
2025-02-05 17:30:19 +0100

    cambiado el campo nombre a name para mejor funcionamiento de la aplicacion

--- models/persona.py
+++ models/persona.py
@@ -7,7 +7,7 @@ class Persona(models.Model):
     _name = 'futbol_master.persona'
     _description = 'Persona'
 
-    nombre = fields.Char(string="Nombre")
+    name = fields.Char(string="Nombre")
     edad = fields.Integer(string="Edad", required=True)
     genero = fields.Selection([ ("1","Masculino"), ("2","Femenino")],string="Genero")
     pais = fields.Selection([
