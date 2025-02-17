1b416a8348dd026d1bfa7892b845db56bfecc100
samuelrubiodev <samueldynamo11@gmail.com>
2025-02-17 19:55:43 +0100

    Mejora vistas

--- models/club.py
+++ models/club.py
@@ -9,16 +9,3 @@ class Club(models.Model):
     name = fields.Char()
     equipo_id = fields.One2many("futbol_master.equipo", "club_id", string="Equipos")
     fechaCreacion = fields.Date(string="Fecha de Creacion", readonly=True, default=datetime.today())
-    pais = fields.Many2one('res.country', string="País")
-    provincia = fields.Many2one('res.country.state', string="Provincia",required=True, 
-                    domain=([('res.country.state.country_id', '=', '0')]), 
-                    attrs="{'readonly': [('pais', '=', False)]}")
-
-    @api.onchange('pais')
-    def _cambio_provincia(self):
-        self.provincia = False
-        return {
-            'domain': {
-                'provincia': [('country_id', '=', self.pais.id)]
-            }
-        }
